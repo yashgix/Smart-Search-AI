e76b7dde1d438639761fd3a49115b48562de7109
yashgix <125082291+yashgix@users.noreply.github.com>
2025-03-24 18:13:30 -0400

    Delete Smart Search AI directory

--- Smart Search AI/backend/llm_engine/call_gemini.py
+++
@@ -1,7 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyBY7MuR7o5nmk-Vys7B0gRYqyb-PWrhLwQ")
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="top sports channel provider comparison in philadelphia? shortly tell"
-)
-print(response.text)
